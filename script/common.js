705d30f7d7df04331476e89dd99d2ba9e18d0805
Thynson <lanxingcan@gmail.com>
2012-06-17 14:53:28 +0800

    Put code into common.js

---
+++ script/common.js
@@ -0,0 +1,30 @@
+;(function(){
+
+	postJson = function(params) {
+		return $.ajax({
+			type : "POST",
+			url : params.url,
+			dataType : "json",
+			data : params.data,
+			success : params.callback,
+			error : params.error
+		});
+	};
+
+	getJson = function(params) {
+		return $.ajax({
+			type : "GET",
+			url : params.url,
+			success : params.callback,
+			error : params.error
+		});
+	};
+
+
+	ajaxSubmit = function(form, callback){
+		form.submit(function(e){
+			callback();
+			e.preventDefault();
+		});
+	};
+})();
--- script/login.js
+++ script/login.js
@@ -1,18 +1,23 @@
 
 ;(function(){
-	$("#loginForm").submit(function(e){
-		$.post("/login", $("#loginForm").serialize(), function(obj) {
-			obj = obj || {};
-			if (obj.err) {
-				$("#info").text(obj.err);
-			} else if (obj.type == "student") {
-			} else if (obj.type == "professor") {
-			} else if (obj.type == "admin") {
-			} else {
+	ajaxSubmit($("#loginForm"), function(){
+		postJson({
+			url : "/login",
+			data : $("#loginForm").serialize(),
+			callback : function(obj){
+				if (obj.err) {
+					$("#info").text(obj.err);
+				} else if (obj.type == "student") {
+				} else if (obj.type == "professor") {
+				} else if (obj.type == "admin") {
+				} else {
+					$("#info").text("系统异常");
+				}
+			},
+			error : function() {
 				$("#info").text("系统异常");
 			}
-		}, "json");
-		e.preventDefault();
-	});
+		});
+	}));
 
 })();
