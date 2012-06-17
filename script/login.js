51619447b16eb9287e821a66bbb636de3d333af7
trinity <imandry.c@gmail.com>
2012-06-17 20:51:58 +0800

    Tweaks on User Interface

--- script/login.js
+++ script/login.js
@@ -1,5 +1,8 @@
 
 ;(function($){
+    $(".close").click(function(){
+        $(".alert").hide("fast");
+    });
 	ajaxSubmit($("#loginForm"), function(){
         var username = $("#username").val();
         var password = $("#password").val();
@@ -15,15 +18,13 @@
 			callback : function(obj){
 				if (obj.err) {
 					$("#alertText").text(obj.err);
+                    $(".alert").show("fast");
 				} else {
 					location.href = "/panel.html";
 				}
 			},
 			error : function() {
-				$("#alertText").text("系统异常");
-                $(".close").click(function(){
-                    $(".alert").hide("fast");
-                });
+				$("#alertText").text("系统异常，请检查您的网络连接或联系管理员");
                 $(".alert").show("fast");
 			}
 		});
