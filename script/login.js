3357ac3830d29a27a7c5de5f2073a79c7aefcb05
Thynson <lanxingcan@gmail.com>
2012-06-25 10:28:59 +0800

    Complete login logic

--- script/login.js
+++ script/login.js
@@ -1,9 +1,17 @@
 
 ;(function($){
 	ajaxSubmit($("#loginForm"), function(){
+        var username = $("#username").val();
+        var password = $("#password").val();
+        var hash = $.sha1(password+$.sha1(username));
+        var postData = "username="
+            + encodeURIComponent(username)
+            + "&password="
+            + encodeURIComponent(hash);
+
 		postJson({
 			url : "/login",
-			data : $("#loginForm").serialize(),
+			data : postData,
 			callback : function(obj){
 				if (obj.err) {
 					$("#alertText").text(obj.err);
