8b4edf41f838e9b239bedd898d2a6b212bff92e3
Thynson <lanxingcan@gmail.com>
2012-06-30 15:31:44 +0800

    use common alert function

--- script/login.js
+++ script/login.js
@@ -1,8 +1,7 @@
 
 ;(function($){
     var errHandler = function() {
-        $("#alertText").text("系统异常，请检查您的网络连接或联系管理员");
-        $(".alert").show("fast");
+        alertInternalError(".alert");
     };
 
     ajaxSubmit($("#loginForm"), function(){
@@ -20,8 +19,7 @@
             callback : function(obj){
                 try {
                     if (obj.err) {
-                        $("#alertText").text(obj.err);
-                        $(".alert").show("fast");
+                        alertFailure(".alert", obj.err);
                     } else {
                         location.href = "/panel.html";
                     }
