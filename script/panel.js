d38f437dce56112b712cedfda4421f0224989652
trinity <imandry.c@gmail.com>
2012-07-02 02:38:32 +0800

    intelligent notify and fix on student select table

--- script/panel.js
+++ script/panel.js
@@ -322,40 +322,45 @@
             var td = $("<td/>").appendTo(tr);
 
             if (s.selected_by) {
-                infoTd.append("选择该课程的学生：");
-                $.each(s.selected_by, function(i, u){
-                    var splitter = "、 ";
 
-                    if (i != 0) {
-                        infoTd.append(splitter);
-                    }
+                if(!s.selected_by.length) {
+                    infoTd.append("该项目尚未有学生选报");
+                }else{
+                    infoTd.append("选择该课程的学生：");
+                    $.each(s.selected_by, function(i, u){
+                        var splitter = "、 ";
 
-                    $("<a/>").text(u.realname)
-                        .attr("href", "#")// + s.username)
-                        .appendTo(infoTd)
-                        .click(function(){
-                            var postdata = "subject=" + encodeURIComponent(s.id)
-                            + "&student=" + encodeURIComponent(u.username);
-                            postJson({
-                                url : "/approve",
-                                data : postdata,
-                                callback : function(obj) {
-                                    if (obj.err) {
-                                        // TODO
-                                    } else {
-                                        td.empty();
-                                        var p = $("<p/>").text("已选择:").appendTo(td);
-                                        $("<b/>").text(u.realname).appendTo(p);
-                                        p = $("<p/>").text("其他选择了这个课题的人数：").appendTo(td);
-                                        $("<b/>").text(s.selected_by.length - 1).appendTo(p);
+                        if (i != 0) {
+                            infoTd.append(splitter);
+                        }
+
+                        $("<a/>").text(u.realname)
+                            .attr("href", "#")// + s.username)
+                            .appendTo(infoTd)
+                            .click(function(){
+                                var postdata = "subject=" + encodeURIComponent(s.id)
+                                + "&student=" + encodeURIComponent(u.username);
+                                postJson({
+                                    url : "/approve",
+                                    data : postdata,
+                                    callback : function(obj) {
+                                        if (obj.err) {
+                                            // TODO
+                                        } else {
+                                            td.empty();
+                                            var p = $("<p/>").text("已选择:").appendTo(td);
+                                            $("<b/>").text(u.realname).appendTo(p);
+                                            p = $("<p/>").text("其他选择了这个课题的人数：").appendTo(td);
+                                            $("<b/>").text(s.selected_by.length - 1).appendTo(p);
+                                        }
+                                    },
+                                    error : function(obj) {
                                     }
-                                },
-                                error : function(obj) {
-                                }
-                            });
+                                });
 
-                        });;
-                });
+                            });;
+                    });
+                }
             }
 
             if (s.selected_by.length != 0 && s.applied_to){
@@ -370,7 +375,7 @@
                 var p = $("<p/>").text("选择了这个课题的人数：").appendTo(td);
                 $("<b/>").text(s.selected_by.length).appendTo(p);
             } else {
-                var p = $("<p/>").text("无人报选");
+                var p = $("<p/>").text("无人报选").appendTo(td);
 
             }
         }
