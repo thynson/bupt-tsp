f4e98a7e2556423643be8a26a9a3f30288520b43
Thynson <lanxingcan@gmail.com>
2012-07-04 15:15:41 +0800

    Implement update UI for edit subject

--- script/panel.js
+++ script/panel.js
@@ -165,15 +165,16 @@
                 // professor add or modify subject
                 ajaxSubmit($("#subject-form"), function() {
 
-                    var postdata = "id=" + s.id + "&"
-                        + $("#subject-form").serialize()
-                        + "&type1="
-                        + $('#subject-form div[name="type1"] .active').attr("name")
-                        + "&type2="
-                        + $('#subject-form div[name="type2"] .active').attr("name")
-                        + "&source="
-                        + $('#subject-form div[name="source"] .active').attr("name")
+                    var subject = {
+                        id : s.id,
+                        name : $('#subject-form input[name="name"]').val(),
+                        desc : $('#subject-form textarea[name="desc"]').val(),
+                        type1 : $('#subject-form div[name="type1"] .active').attr("name"),
+                        type2 : $('#subject-form div[name="type2"] .active').attr("name"),
+                        source : $('#subject-form div[name="source"] .active').attr("name"),
+                    };
 
+                    var postdata = serializeObject(subject);
                     postJson({
                         url : "/modify",
                         data : postdata,
@@ -181,7 +182,12 @@
                             if (obj.err) {
                                 alertFailure("#subjectFormAlert", obj.err);
                             } else {
-                                getSubjectDetail();
+                                s.name = subject.name;
+                                s.desc = subject.desc;
+                                s.type1 = subject.type1;
+                                s.type2 = subject.type2;
+                                s.source = subject.source;
+                                //getSubjectDetail();
                                 alertSuccess("#subjectFormAlert", "更新成功");
                             }
                         },
